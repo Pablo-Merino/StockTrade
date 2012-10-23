2c22dcc9f3bd938325f154469b5074c72c6336dc
Pablo Merino <pablo.perso1995@gmail.com>
2012-10-23 20:37:17 +0200

    some fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,21 +23,43 @@ $(function() {
 	$('.portfolio_chart span').peity('line', { height: 50, width:200, min: $('.porftolio_chart span').data('min')});
 
 	updateShareTotalCost();
+	portfolioShareCost();
 	$('#share_amount').on('change keyup', function() {
 		if($('#share_amount').attr('value') > 0)
 			updateShareTotalCost();
 	});
 
+
 	function updateShareTotalCost() {
-		$('p.estimation').text(parseInt($('#share_amount').attr('value')));
+		$('.cost_estimation p.estimation').text(parseInt($('#share_amount').attr('value')));
 		if(parseInt($('#share_amount').attr('value')) == 1) {
-			$('p.estimation').append(" share");
+			$('.cost_estimation p.estimation').append(" share");
 		} else {
-			$('p.estimation').append(" shares");
+			$('.cost_estimation p.estimation').append(" shares");
 		}
 			
-		$('p.estimation').append(" would cost you "+accounting.formatMoney(parseInt($('#share_amount').attr('value')) * parseInt($('div.cost_estimation').data('share_cost'))/100));
+
+		$('.cost_estimation p.estimation').append(" would cost you "+accounting.formatMoney(parseInt($('#share_amount').attr('value')) * parseInt($('div.cost_estimation').data('share_cost'))/100));
+
+
 	}
+
+	function portfolioShareCost() {
+		$('.company_list').children().each(function () {
+
+			$('.win_estimation p.estimation', this).append(" would get you "+accounting.formatMoney(parseInt($('#share_amount', this).attr('value')) * parseInt($('div.cost_estimation', this).data('share_cost'))/100));
+
+			$('.win_estimation p.estimation', this).text(parseInt($('#share_amount', this).attr('value')));
+			if(parseInt($('#share_amount', this).attr('value')) == 1) {
+				$('.win_estimation p.estimation', this).append(" share");
+			} else {
+				$('.win_estimation p.estimation', this).append(" shares");
+			}
+				
+			$('.win_estimation p.estimation', this).append(" would get you "+accounting.formatMoney(parseInt($('#share_amount', this).attr('value')) * parseInt($('div.win_estimation', this).data('share_cost'))/100));
+		});
+	}
+
 
 });
 
